a566191d2947288af0c40e521d5a2bee7500a363
Kenyon Ralph <kenyon@kenyonralph.com>
2020-12-01 01:29:28 -0800

    day 1 part 2 in ruby

--- 1-Report_Repair.rb
+++ 1-Report_Repair.rb
@@ -3,6 +3,8 @@
 
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+# usage: ./1-Report_Repair.rb input-day1
+
 entries = []
 
 ARGF.each do |entry|
@@ -11,11 +13,31 @@ end
 
 entries.sort!
 
+puts 'part 1'
+done = false
 entries.each do |e1|
   entries.reverse_each do |e2|
     if e1 + e2 == 2020
       puts "entry1: #{e1}, entry2: #{e2}, sum: #{e1 + e2}, product: #{e1 * e2}"
-      exit
+      done = true
+      break
     end
+    break if done
   end
+end
+
+puts 'part 2'
+done = false
+entries.each_index do |i|
+  entries.drop(i + 1).each do |e1|
+    entries.reverse_each do |e2|
+      next unless entries[i] + e1 + e2 == 2020
+
+      puts "entries[i]: #{entries[i]}, entry1: #{e1}, entry2: #{e2}, sum: #{entries[i] + e1 + e2}, product: #{entries[i] * e1 * e2}"
+      done = true
+      break
+    end
+    break if done
+  end
+  break if done
 end
